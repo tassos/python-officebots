9d1d6497d9c4ba28641aa7a2c1e7715140105d64
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-11-27 20:50:49 +0000

    [setup.py] Fix package list

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://git.brl.ac.uk/s-lemaignan/officebots-python",
-    packages=setuptools.find_packages(),
+    package_dir = {"":"src"},
+    packages=["officebots"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
