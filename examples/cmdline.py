596a26211872bd4bb08eb68f9228059123456e6d
Séverin Lemaignan <severin.lemaignan@uwe.ac.uk>
2020-11-30 17:48:39 +0000

    Implement set-screen in cmdline.py

--- examples/cmdline.py
+++ examples/cmdline.py
@@ -5,6 +5,10 @@ logging.basicConfig(level=logging.INFO)
 
 import sys
 
+# libraries to load and encode JPG images
+import pathlib
+import base64
+
 from officebots import Robot
 
 logging.getLogger('officebots').setLevel(logging.DEBUG)
@@ -20,10 +24,18 @@ cmd = [sys.argv[1], sys.argv[2], [x for x in sys.argv[3:]]]
 
 class MyRobot(Robot):
 
+    def load_jpg(self, path):
+        with open(path, 'rb') as img:
+            raw = img.read()
+
+        return base64.b64encode(raw).decode("ascii")
 
     async def run(self):
 
-        #while True:
+        # if calling 'set-screen', we must first load the jpg image
+        # and encode it in base64
+        if cmd[1] == "set-screen":
+            cmd[2] = [self.load_jpg(cmd[2][0])]
 
         response = await self.execute(cmd)
 
