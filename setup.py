37d23f6c2ab776237bff68e20d1e33da1b609845
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-10-11 09:37:31 +0200

    bumped to 0.2.0

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="officebots",
-    version="0.1.0",
+    version="0.2.0",
     author="Séverin Lemaignan",
-    author_email="severin.lemaignan@brl.ac.uk",
+    author_email="severin.lemaignan@pal-robotics.com",
     description="Python API for the OfficeBots game",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://git.brl.ac.uk/s-lemaignan/officebots-python",
-    package_dir = {"":"src"},
+    url="https://github.com/severin-lemaignan/python-officebots",
+    package_dir={"": "src"},
     packages=["officebots"],
-    scripts = ["scripts/officebots-ros"],
+    scripts=["scripts/officebots-ros"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
